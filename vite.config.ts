f800b3bd670bb69a984b8e561c95e6c06195099c
Vaibhav Mule <vaibhavmule135@gmail.com>
2025-04-01 03:20:06 +0530

    add the configurations

--- vite.config.ts
+++ vite.config.ts
@@ -12,10 +12,19 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        format: 'es',
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
       },
     },
+    sourcemap: true,
   },
   server: {
     port: 3000,
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
   },
 });
